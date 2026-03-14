32292386214741d163688880c3adb16c30adc41b
seaG7 <neyro.yt@gmail.com>
2026-03-14 04:24:16 +0300

    practice1

---
+++ Lesson1/buttonclicker/src/main/java/ru/mirea/danilovma/buttonclicker/MainActivity.java
@@ -0,0 +1,56 @@
+package ru.mirea.danilovma.buttonclicker;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewStudent;
+    private Button btnWhoAmI;
+    private Button btnItIsNotMe;
+    private CheckBox checkBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        textViewStudent = findViewById(R.id.tvOut);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        btnItIsNotMe = findViewById(R.id.btnItIsNotMe);
+        checkBox = findViewById(R.id.checkBox);
+
+        View.OnClickListener oclBtnWhoAmI = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewStudent.setText("Мой номер по списку № 5");
+                checkBox.setChecked(true);
+            }
+        };
+
+        btnWhoAmI.setOnClickListener(oclBtnWhoAmI);
+    }
+
+    public void onMyButtonClick(View view) {
+        textViewStudent.setText("Это не я сделал");
+        checkBox.setChecked(false);
+        Toast.makeText(this, "Ещё один способ!", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ Lesson1/control_lesson1/src/main/java/ru/mirea/danilovma/control_lesson1/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.mirea.danilovma.control_lesson1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+    }
+}
